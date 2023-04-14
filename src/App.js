a9f91e636d66bc49840799395f8e823c4529f578
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-04-14 19:31:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("software");
+
+  useEffect(() => {
+   
+     fetch(
+        `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=Y-OOXtwlVEcx_poIg3l69XvusdENNDQp-XjgFZaAorM`
+      ).then((res)=>res.json()).then((data)=>setFetchData(data.results))
+     
+    
+  }, [query]);
+ 
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+   
+  };
+  function getSearch(e) {
+    e.preventDefault();
+    setQuery(search);
+
+    setSearch("");
+  }
+
+  return (
+    <div className="App">
+      <form className="" onSubmit={getSearch}>
+        <input
+          className=""
+          id="input"
+          type="search"
+          placeholder="Search Image Name..."
+          aria-label="Search"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button id="btn" className="" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="container">
+        {fetchData.map((item, key) => {
+        
+
+          return (
+            <div className="pictures" key={key}>
+              <img
+                src={item.urls.small}
+                style={{ width: "100%",height:"auto" }}
+                alt="images"
+              />
+              <a href={item.links.download} target="_blank" rel="noreferrer">
+                download
+              </a>
+              <h6>user :{item.user.name}</h6>
+              <h6>created_at :{item.created_at}</h6>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
